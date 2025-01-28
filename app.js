67c89e54320f833fdf7a9aa1624638038f2e9147
KriApps <krishac21@gmail.com>
2025-01-28 06:15:41 +0000

    working add, new comments page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname + '/client')));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+
 
 const storage = multer.diskStorage({
 destination: (req, file, cb) => {
@@ -31,19 +31,13 @@ app.post('/api/artpics', upload.single('art'),(req,res)=>{
     if (!title|| !artist || !description) {
         return res.status(400).json({ error: 'All fields must be filled out' });
     }
-
-    const artMeta = {
-        title : req.body.title ,
-        artist : req.body.artist,
-        description : req.body.description,
-        imagePath : req.file.path,
-        
-    }
     
 
     fs.readFile('artgallery.json', (err, data) => {
         if (err) {
           console.log(err)
+
+          
 
           if (err.code === "ENOENT"){
             artList = [artMeta]
@@ -57,15 +51,22 @@ app.post('/api/artpics', upload.single('art'),(req,res)=>{
           }
           
         } else {
-          console.log(data)
-          console.log(artMeta)
+
           const artList = JSON.parse(data);
-          
+          let idCounter = artList.length+1 ;
+          const artMeta = {
+            artId : idCounter,
+            title : req.body.title ,
+            artist : req.body.artist,
+            description : req.body.description,
+            imagePath : '/images/'+req.file.filename,
+        }
+          //console.log(idCounter)
           artList.push(artMeta);
-          console.log(artList)
+          
 
           fs.writeFile('artgallery.json', JSON.stringify(artList, null, 2), (err) => {
-            if (err) {
+            if (err) { 
                 return res.status(500).json({error : 'Cannot save to JSON'})
             }
             res.json({message: 'Art metadata stored in JSON file'})
@@ -77,7 +78,87 @@ app.post('/api/artpics', upload.single('art'),(req,res)=>{
     })
 });
 
-//app.get()
+app.get('/artgallery', (req, res) => {
+    res.sendFile(path.join(__dirname , 'artgallery.json'));
+});
+
+app.get('/artgallery/:id', (req, res) => {
+  const artId = parseInt(req.params.id); 
+
+  fs.readFile('artgallery.json', (err, data)=>{
+    if (err){
+        return res.status.json({error:'File not found'})
+    }else{
+        const artList = JSON.parse(data);
+        const artObject = artList.find(art => art.artId === artId);
+        
+
+        if (!artObject) {
+            return res.status(404).json({ error: 'Art not found' }); 
+          }
+
+        return res.json(artObject)
+    }
+  })
+
+  
+});
+
+/*
+//for comments
+app.post('/api/artcomments', (req,res)=>{
+    if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded.' });
+    }
+
+    const { commentMaker , comment , artofCommentId} = req.body;
+    if (!commentMaker|| !comment || !artofCommentId) {
+        return res.status(400).json({ error: 'All fields must be filled out' });
+    }
+    
+
+    fs.readFile('artgallery.json', (err, data) => {
+        if (err) {
+          console.log(err)
+
+          if (err.code === "ENOENT"){
+            artComments = [artMeta]
+
+            fs.writeFile('artgallery.json', JSON.stringify(artList, null), (err, data)=>{
+                if (err) {
+                    return res.status(500).json({error : 'Cannot save to JSON'})
+                }
+                res.json({message: 'Art metadata stored in JSON file'})
+            })
+          }
+          
+        } else {
+
+          const artList = JSON.parse(data);
+          let idCounter = artList.length+1 ;
+          const artMeta = {
+            artId : idCounter,
+            title : req.body.title ,
+            artist : req.body.artist,
+            description : req.body.description,
+            imagePath : '/images/'+req.file.filename,
+        }
+          //console.log(idCounter)
+          artList.push(artMeta);
+          
+
+          fs.writeFile('artgallery.json', JSON.stringify(artList, null, 2), (err) => {
+            if (err) { 
+                return res.status(500).json({error : 'Cannot save to JSON'})
+            }
+            res.json({message: 'Art metadata stored in JSON file'})
+          });
+        }
+
+    
+
+    })
+})*/
 
 app.use((req, res)=>{
     res.status(404);
--- client/index.js
+++ client/index.js
@@ -12,6 +12,8 @@ if (artFormContent) {
 }
 });
 
+
+
 let forms = document.querySelectorAll('.artpics-form');
 console.log(forms)
 
@@ -63,7 +65,7 @@ document.getElementById('add-art').addEventListener('click', ()=>{
 
 function fetchArt(){
     forms.forEach((form)=>{
-        console.log(form)
+        
         form.addEventListener('submit', async function(event){
         event.preventDefault();
 
@@ -86,7 +88,8 @@ function fetchArt(){
             alert("Art uploaded successfully!")
             formCont.innerHTML = '';
             localStorage.setItem('artFormContent', '');
-            
+            displayAddedArt();
+
         }else{
             const error = await response.text();
             alert("Server could not submit form", error)
@@ -102,3 +105,203 @@ function fetchArt(){
 
 fetchArt();
 
+async function displayArt(){
+
+    try{
+      const response = await fetch('/artgallery');
+      
+      if (response.ok){
+          const result = await response.json();
+          
+          document.getElementById('artPic-container').innerHTML =  `
+          <div id="artgallery-container" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 art-gallery-css">
+          </div>
+          `;
+          const artgallery = document.getElementById('artgallery-container')
+
+
+          result.forEach(artMeta => {
+              artgallery.innerHTML+=`
+                <div class="col">
+                <div class="card shadow-sm">
+                  <img class="bd-placeholder-img card-img-top" width="100%" height="400" xmlns="http://www.w3.org/2000/svg" src="${artMeta.imagePath}"  aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                  <div class="card-body">
+                    <h2>${artMeta.title}</h2>
+                    <p class="card-text">${artMeta.description}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" onclick = "viewArt('${artMeta.artId}');">View</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              `;
+            })
+            
+
+            
+        }else{
+            const error = await response.text();
+            alert("Server could not submit form", error)
+
+        }
+        
+    } catch (error){
+      console.log("error fetching JSON data", error)
+    }
+  };
+
+displayArt();
+
+async function displayAddedArt(){
+
+    try{
+      const response = await fetch('/artgallery');
+      
+      if (response.ok){
+          const result = await response.json();
+          
+          const artgallery = document.getElementById('artgallery-container')
+
+          const artObject = result[result.length-1];
+
+          
+          artgallery.innerHTML += `
+          <div class="col">
+          <div class="card shadow-sm">
+            <img class="bd-placeholder-img card-img-top" width="100%" height="400" xmlns="http://www.w3.org/2000/svg" src="${artObject.imagePath}"  aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+            <div class="card-body">
+              <h2>${artObject.title}</h2>
+              <p class="card-text">${artObject.description}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick = "viewArt('${artObject.artId}');">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+
+            
+        }else{
+            const error = await response.text();
+            alert("Server could not submit form", error)
+
+        }
+        
+    } catch (error){
+      console.log("error fetching JSON data", error)
+    }
+  };
+
+  
+
+async function viewArt(id){
+
+    try{
+        const response = await fetch(`/artgallery/${id}`);
+        
+        if (response.ok){
+            const artObject = await response.json();
+            
+            const artPost = document.getElementById('artPic-container');
+
+            artPost.innerHTML = `
+                <button class = "back-btn" id = "artpic-back-btn" style="font-size: 60px; font-weight : 500" onclick = "
+                displayArt();
+                "><</button>
+                <p style = "font-size: 60px; font-weight : 500" >${artObject.title}</p>
+                <p style = "font-size: 25px; font-weight : 90" >Art by ${artObject.artist}</p>
+                <p>${artObject.description}</p>
+
+                <img  style="width: 50%; height: auto" class="view-artpic" id="view-art" src = "${artObject.imagePath}">
+                <br><br>
+
+                <div id = "comments-container" class ="comments-container">
+                <h1>Comments</h1><br>
+                
+                <form method = "post" class="comments-form" id = "comments-form">
+                    <input class="comment" id="comment-maker" name="commentMaker" placeholder="Enter your name" type="text"><br><br>
+                    <input class="comment" id = "comment" name="comment" placeholder="What are your thoughts?" type = "text"><br><br>
+                    <input type="hidden" name="artofCommentId" value = "${artObject.artId}">
+                    <button class = "submit-comment" id="submit-comment-btn" type="submit">Send</button>
+                </form>
+
+
+                </div>
+            `;
+              
+          }else{
+              const error = await response.text();
+              alert("Server could not show image", error)
+  
+          }
+          
+      } catch (error){
+        console.log("error fetching JSON data", error)
+      }
+    };
+
+/*
+    document.addEventListener('DOMContentLoaded', () => {
+        const artFormContent = localStorage.getItem('artFormContent');
+        if (artFormContent) {
+            formCont.innerHTML = artFormContent;
+            forms = document.querySelectorAll('.artpics-form');
+            console.log(forms)
+            fetchArt();
+        }
+        });
+  
+    let commentsForms = document.querySelectorAll('.comments-form');
+    console.log(commentsForms)
+
+    function postComments(){
+        commentsForms.forEach((commentForm)=>{
+            
+            commentForm.addEventListener('submit', async function(event){
+            event.preventDefault();
+    
+            
+            const commentFormData = new FormData(commentForm);
+            
+            console.log('form data', commentFormData)
+            
+            
+            try {
+            
+                const response = await fetch('/api/artcomments', {
+                    method: 'POST',
+                
+                    body : commentFormData
+                });
+    
+            if (response.ok){
+                const result = await response.json();
+                alert("Art uploaded successfully!")
+                //formCont.innerHTML = '';
+                localStorage.setItem('commentFormContent', '');
+                displayAddedArt();
+    
+            }else{
+                const error = await response.text();
+                alert("Server could not submit form", error)
+    
+            }
+            }catch (error){
+            console.log('error', error)
+            alert("Error submitting form")
+            }
+        })
+        })
+    };
+    
+    fetchArt();
+
+
+*/
+    
+
+  
